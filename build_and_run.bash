40d8c75f27010602d6f4ad5a319692ca5014c0ab
James Shaw <penguinoneshaw@gmail.com>
2019-01-25 10:54:03 +0000

    Fix the failed assumption that exit is a function in bash...

--- build_and_run.bash
+++ build_and_run.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd build;
-cmake3 --build . --config Release || exit(1);
+cmake3 --build . --config Release || exit 1;
 cd ..;
 stat output && mv output output_prev;
-build/ProjectModelling data || exit(1);
+OMP_NUM_THREADS=4 build/ProjectModelling data || exit 1;
 tar czf output.tar.gz output
 echo "Results from data run done `date`" | mail -a output.tar.gz -s "project run complete" s1410767
-exit(0);
+exit 0;
